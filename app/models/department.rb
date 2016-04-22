c319db11c120a769f6e3402c9aeceb9567e6daca
Cuauhtemoc Solis Ventura <cuauhtemoc.solis@ngn-mobile.com.mx>
2016-04-21 22:22:03 -0500

    I made some changes into the form error messages, The message was moved to partials/_error_alert Also, i added the Moments link to the navigation

--- app/models/department.rb
+++ app/models/department.rb
@@ -1,2 +1,6 @@
 class Department < ActiveRecord::Base
+		# Validations
+	validates :name, presence: true, uniqueness:true,length:{minimum:3}
+	validates :description, presence: true
 end
+
--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,3 +1,6 @@
 class Employee < ActiveRecord::Base
   belongs_to :department
+  validates :first_name,presence: true
+  validates :last_name,presence: true
+  validates :mail,presence: true,uniqueness:true
 end
