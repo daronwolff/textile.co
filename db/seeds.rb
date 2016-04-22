cb60ae19020e65bf3bd74d1e394006c0e32a0019
Cuauhtemoc Solis Ventura <cuauhtemoc.solis@ngn-mobile.com.mx>
2016-04-22 03:39:14 -0500

    I added default user and default moments to the database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,2 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+moments = Moment.create([{name:'Arrival time'},{name:'Lunch start'},{name:"Lunch end"},{name:"Depart Time"}])
+admin = Admin.create!  :email => 'admin@textil.co', :password => 'admin123', :password_confirmation => 'admin123'
